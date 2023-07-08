534adaa3bef41d0758f1a7482df08186a6cd9d20
Aniketh <aaatipamula@gmail.com>
2023-07-08 15:29:24 +0000

    changes to prompt

--- src/anikethChain.py
+++ src/anikethChain.py
@@ -6,16 +6,17 @@ from langchain.prompts import PromptTemplate
 
 # Modify to change behavior of AI
 template = """
-Bob is a thoughtful, random, and quirky person.
-Bob often writes his thoughts about random experiences in his day to day life. 
+Aniketh is a thoughtful, random, and quirky person.
+Aniketh often writes his thoughts about random experiences in his day to day life. 
 
 Here are a few examples:
 
-Bob was listening to a song and thought "this song is pretty good, not perfect but good enough for the movie I think"
-Bob saw a lexus lfa and thought "god the things I would do to drive that car"
-Bob was using a new custom mechanical keyboard and thought "I really like the feel of these new switches, they're not too heavy and super smooth"
+Aniketh saw a lexus lfa and thought "god the things I would do to drive that car"
+Aniketh was using a new custom mechanical keyboard and thought "I like the feel of these new switches, they're not too heavy and super smooth, but they could benefit from some break in"
+Aniketh was working on a project for an internship and had a breakthrough on some code and thought "oh my god it finally works, python is so wack sometimes but also I'm kinda dumb sometimes"
+Aniketh was watching an F1 race and thought "wow verstappens gonna win another one, what a surprise"
 
-Bob writes sentences with all lowercase letters and often uses abbreviations such as:
+Aniketh writes sentences with all lowercase letters and often uses abbreviations such as:
 
 "idk" for "i don't know"
 "bro" for "brother"
@@ -24,17 +25,20 @@ Bob writes sentences with all lowercase letters and often uses abbreviations suc
 "damn" for "dang" or "darn"
 "nvm" for "nevermind"
 "gonna" for "going"
+"yk" for "you know"
 
-Write a thought about {topic} in the style of Bob.
+Aniketh does not puntuate his sentences except for the use of commas.
+
+Write a thought about {topic} in the style of Aniketh.
 """
 
 prompt = PromptTemplate(template=template, input_variables=["topic"])
 
-def create_aniketh_ai(temprature: Optional[float] = 0.9, verbose: Optional[bool] = None) -> LLMChain:
+def create_aniketh_ai(temperature: Optional[float] = 0.9, verbose: Optional[bool] = None) -> LLMChain:
     """Create a chain to craft a response similar to me"""
 
     verbose = verbose if verbose else False
-    llm = OpenAI(temperature=temprature)
+    llm = OpenAI(temperature=temperature)
 
     chain = LLMChain(
         llm=llm,
